6ddbb3bf222500527cbd4f598768fb02988c824a
Amaury Diaz <amauryuh@gmail.com>
2024-02-26 22:01:00 +0100

    Release v0.2.37

--- main.go
+++ main.go
@@ -81,6 +81,7 @@ func exposeMapBuilders(g *protogen.GeneratedFile, f *protogen.File, messages ...
 		g.P("// LoadMap ...")
 		g.P("func (e *", message.GoIdent, ") LoadMap(m map[string]interface{}) {")
 		for _, field := range message.Fields {
+			g.P("// ", field.Desc.Name())
 			if field.Oneof != nil {
 				continue
 			}
