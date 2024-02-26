48271d8a4e5c7b7ffa49799971baaba857895ad1
Amaury Diaz <amauryuh@gmail.com>
2024-02-26 10:49:39 +0100

    Release v0.0.13

---
+++ descriptor.go
@@ -0,0 +1,12 @@
+package main
+
+import "google.golang.org/protobuf/reflect/protoreflect"
+
+type overrideJsonName struct {
+	name string
+	protoreflect.FieldDescriptor
+}
+
+func (d overrideJsonName) Name() protoreflect.Name {
+	return protoreflect.Name(d.name)
+}
