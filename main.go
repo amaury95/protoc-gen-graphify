c6689f417aa875f2a025d19ad432fffe219de9bf
Amaury Diaz <amauryuh@gmail.com>
2024-02-26 17:10:43 +0100

    Release v0.2.17

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func exposeMapBuilders(g *protogen.GeneratedFile, f *protogen.File, messages ...
 			g.P("if _opt, ok := m[\"" + oneof.GoName + "\"].(map[string]interface{}); ok {")
 			for _, oneofField := range oneof.Fields {
 				g.P("if _val , ok := _opt[\"", oneofField.GoName, "\"].(map[string]interface{}); ok {")
-				g.P("field := new(", oneofField.Oneof.Desc.Name(), ")")
+				g.P("field := new(", oneofField.Oneof.GoIdent, ")")
 				g.P("field.LoadMap(_val)")
 				g.P("e.Type = &", oneofField.GoIdent, "{", oneofField.GoName, ":field}")
 				g.P("}")
