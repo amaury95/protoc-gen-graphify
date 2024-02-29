10bb8415bed1d2ab7ef430f02445a980c8ceb7ed
Amaury Diaz <amauryuh@gmail.com>
2024-02-29 10:48:57 +0100

    Release v0.3.3

--- specs.go
+++ specs.go
@@ -3,30 +3,43 @@ package main
 import "google.golang.org/protobuf/compiler/protogen"
 
 func generateSpecs(g *protogen.GeneratedFile, _ *protogen.File, messages ...*protogen.Message) {
+	g.P()
+	g.P("// Specs ...")
+	g.P("func Specs() []byte {")
+	g.P("var buffer ", g.QualifiedGoIdent(bytesBuffer))
 	for _, message := range messages {
 		if message.Desc.IsMapEntry() {
 			continue
 		}
-		g.P()
-		g.P("// Specs ...")
-		g.P("func (o *", message.GoIdent, ") Specs() []byte {")
-		g.P("var buffer ", g.QualifiedGoIdent(bytesBuffer))
+		P(g, quote(string(message.Desc.Name())), ": {")
+		P(g, quote("fields"), ":{")
 		for _, field := range message.Fields {
 			if field.Oneof != nil && !field.Oneof.Desc.IsSynthetic() {
 				continue
 			}
+			P(g, quote("name"), ":", quote(string(field.Desc.Name())))
 		}
+		P(g, "}")
 		for _, field := range message.Oneofs {
 			if field.Desc.IsSynthetic() {
 				continue
 			}
 		}
-		g.P("return buffer.Bytes()")
-		g.P("}")
+		P(g, "}")
 	}
+	g.P("return buffer.Bytes()")
+	g.P("}")
 }
 
 var bytesBuffer protogen.GoIdent = protogen.GoIdent{
 	GoName:       "Buffer",
 	GoImportPath: "bytes",
+}
+
+func P(g *protogen.GeneratedFile, v ...interface{}) {
+	g.P(join("buffer.WriteString(\"", v, "\")")...)
+}
+
+func quote(val string) string {
+	return "\\\"" + val + "\\\""
 }
