13157af006b541f453b9747e947821b410ee0a66
Amaury Diaz <amauryuh@gmail.com>
2024-02-29 10:08:38 +0100

    Release v0.3.1

---
+++ specs.go
@@ -0,0 +1,25 @@
+package main
+
+import "google.golang.org/protobuf/compiler/protogen"
+
+func generateSpecs(g *protogen.GeneratedFile, _ *protogen.File, messages ...*protogen.Message) {
+	for _, message := range messages {
+		if message.Desc.IsMapEntry() {
+			continue
+		}
+		g.P()
+		g.P("// Specs ...")
+		g.P("func (o *", message.GoIdent, ") Specs() []byte {")
+		for _, field := range message.Fields {
+			if field.Oneof != nil && !field.Oneof.Desc.IsSynthetic() {
+				continue
+			}
+		}
+		for _, field := range message.Oneofs {
+			if field.Desc.IsSynthetic() {
+				continue
+			}
+		}
+		g.P("}")
+	}
+}
