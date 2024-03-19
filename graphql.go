1eb3e6ba16da01b7ba12dc08baf4e3c7ac93fb9d
Amaury Diaz <amauryuh@gmail.com>
2024-03-19 14:08:23 +0100

    Release v0.3.74

---
+++ graphql.go
@@ -0,0 +1,60 @@
+package main
+
+import "google.golang.org/protobuf/compiler/protogen"
+
+func generateObject(g *protogen.GeneratedFile, _ *protogen.File, messages ...*protogen.Message) {
+	g.P(`
+	/*
+		Graphql object
+	*/
+	`)
+	for _, message := range messages {
+		if message.Desc.IsMapEntry() {
+			continue
+		}
+		g.P("\n/* Object ... */")
+		g.P("func (*", message.GoIdent, ") Object() *", g.QualifiedGoIdent(Object), " {")
+		g.P("return ", g.QualifiedGoIdent(NewObject), "(", g.QualifiedGoIdent(ObjectConfig), "{})")
+		g.P("}")
+	}
+}
+
+var Fields protogen.GoIdent = protogen.GoIdent{
+	GoName:       "Fields",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var Field protogen.GoIdent = protogen.GoIdent{
+	GoName:       "Field",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var String protogen.GoIdent = protogen.GoIdent{
+	GoName:       "String",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var Int protogen.GoIdent = protogen.GoIdent{
+	GoName:       "Int",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var Object protogen.GoIdent = protogen.GoIdent{
+	GoName:       "Object",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var NewObject protogen.GoIdent = protogen.GoIdent{
+	GoName:       "NewObject",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var ObjectConfig protogen.GoIdent = protogen.GoIdent{
+	GoName:       "ObjectConfig",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var NewList protogen.GoIdent = protogen.GoIdent{
+	GoName:       "NewList",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
