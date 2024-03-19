7b3861b71aaeb087eda9ad42f8b076d1fc8f8800
Amaury Diaz <amauryuh@gmail.com>
2024-03-19 15:36:33 +0100

    Release v0.4.13

--- graphql.go
+++ graphql.go
@@ -1,6 +1,9 @@
 package main
 
-import "google.golang.org/protobuf/compiler/protogen"
+import (
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
 
 func generateObject(g *protogen.GeneratedFile, _ *protogen.File, messages ...*protogen.Message) {
 	g.P(`
@@ -22,7 +25,14 @@ func generateObject(g *protogen.GeneratedFile, _ *protogen.File, messages ...*pr
 		g.P("Name: \"", message.GoIdent, "\",")
 		g.P("Fields: ", g.QualifiedGoIdent(Fields), "{")
 		for _, field := range message.Fields {
+
 			g.P("\"", field.Desc.Name(), "\":&", g.QualifiedGoIdent(Field), "{")
+			if field.Desc.IsList() {
+				g.P("Type: ", g.QualifiedGoIdent(NewList), "(", getFieldType(g, field), "),")
+			} else {
+				g.P("Type: ", getFieldType(g, field), ",")
+			}
+			g.P()
 			g.P("},")
 		}
 		for _, field := range message.Oneofs {
@@ -60,6 +70,34 @@ func generateObject(g *protogen.GeneratedFile, _ *protogen.File, messages ...*pr
 	}
 }
 
+func getFieldType(g *protogen.GeneratedFile, field *protogen.Field) string {
+	switch field.Desc.Kind() {
+	case protoreflect.MessageKind:
+		// return g.QualifiedGoIdent()
+	case protoreflect.BoolKind:
+		return g.QualifiedGoIdent(Boolean)
+	case protoreflect.EnumKind:
+		// assignEnum(export, g, field, recipient, assign, identifier...)
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		return g.QualifiedGoIdent(Int)
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		return g.QualifiedGoIdent(Int)
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		return g.QualifiedGoIdent(Int)
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return g.QualifiedGoIdent(Int)
+	case protoreflect.FloatKind:
+		return g.QualifiedGoIdent(Float)
+	case protoreflect.DoubleKind:
+		return g.QualifiedGoIdent(Float)
+	case protoreflect.StringKind:
+		return g.QualifiedGoIdent(String)
+	case protoreflect.BytesKind:
+		return g.QualifiedGoIdent(String)
+	}
+	return ""
+}
+
 var Fields protogen.GoIdent = protogen.GoIdent{
 	GoName:       "Fields",
 	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
@@ -77,6 +115,26 @@ var String protogen.GoIdent = protogen.GoIdent{
 
 var Int protogen.GoIdent = protogen.GoIdent{
 	GoName:       "Int",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var Float protogen.GoIdent = protogen.GoIdent{
+	GoName:       "Float",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var Boolean protogen.GoIdent = protogen.GoIdent{
+	GoName:       "Boolean",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var ID protogen.GoIdent = protogen.GoIdent{
+	GoName:       "ID",
+	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
+}
+
+var DateTime protogen.GoIdent = protogen.GoIdent{
+	GoName:       "DateTime",
 	GoImportPath: protogen.GoImportPath("github.com/graphql-go/graphql"),
 }
 
