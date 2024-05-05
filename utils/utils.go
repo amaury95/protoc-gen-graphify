ad00cc8d6cee84951035038a9276fbfe44c4bc72
Amaury Diaz <amauryuh@gmail.com>
2024-05-05 16:28:00 +0200

    Release v0.6.28

--- utils/utils.go
+++ utils/utils.go
@@ -50,34 +50,6 @@ func MapFromBytes(data []byte) (res map[string]interface{}, err error) {
 	return
 }
 
-// Unmarshaler ...
-type Unmarshaler interface {
-	UnmarshalMap(m map[string]interface{})
-}
-
-// Message ...
-type Message interface {
-	Schema() map[string]interface{}
-}
-
-// GraphqlObject ...
-type GraphqlObject interface {
-	// Object ...
-	Object() *graphql.Object
-}
-
-// GraphqlArgument ...
-type GraphqlArgument interface {
-	// Argument ...
-	Argument() graphql.FieldConfigArgument
-}
-
-// GraphqlOutput ...
-type GraphqlOutput interface {
-	// Output ...
-	Output() graphql.Output
-}
-
 // JSON json type
 var JSON = graphql.NewScalar(
 	graphql.ScalarConfig{
